1c0651762edc2675bb6c3859ee7a2b8203db47b8
Sean Blake <sean.blake97@outlook.com>
2022-07-09 09:49:12 -0400

    The first commit, created project.

---
+++ The Saloon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Saloon
+//
+//  Created by SEAN BLAKE on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Ucleus!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Saloon/The_SaloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_SaloonApp.swift
+//  The Saloon
+//
+//  Created by SEAN BLAKE on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct The_SaloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
